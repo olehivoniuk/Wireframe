027e418fe7d06751914aa2e0dd6659166fc00ce5
olehivoniuk <99249589+olehivoniuk@users.noreply.github.com>
2022-11-29 00:21:33 +0200

    added apiUrl plus forecast for each day of the week

--- src/index.js
+++ src/index.js
@@ -21,26 +21,42 @@ let day = days[now.getDay()];
 let lineDate = document.querySelector("#currentTime");
 lineDate.innerHTML = day + " " + hours + ":" + minutes;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
 
 <div class="col-2">
-   <div class="weather-forecast-date">${day}</div>
-   <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="weather image" class="icon1"/>
+   <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+   <img src="http://openweathermap.org/img/wn/${
+     forecastDay.weather[0].icon
+   }@2x.png" alt="weather image" class="icon1"/>
 <div class="weather-forecast-temperatures">
-  <span class="weather-forecast-temperature-max">21° </span>
-  <span class="weather-forecast-temperature-min">- 12°</span>
+  <span class="weather-forecast-temperature-max">${Math.round(
+    forecastDay.temp.max
+  )}° </span>
+  <span class="weather-forecast-temperature-min">- ${Math.round(
+    forecastDay.temp.min
+  )}°</span>
 </div>
 </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
